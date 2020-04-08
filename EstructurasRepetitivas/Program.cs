3ef8d9208fd75ca018c86916f34d3b736d13daa1
Fernando Bruzzo <fernandobruzzo7@gmail.com>
2020-04-08 20:48:11 -0300

    nueva solucion

---
+++ EstructurasRepetitivas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasRepetitivas
+{
+    /*
+     * Objetivo: Comprender el uso básico de estructuras repetitivas.
+       Ejercicio:
+       Calcular la media y la desviación estándar de un conjunto de 10 personas.
+       Tome por teclado la altura en cm de cada persona y cárguela en un arreglo.
+       Presente los resultados obtenidos.
+       Muestre que alturas se encuentran por encima de la media y por debajo de ella.
+       Muestre que alturas se encuentran dentro del rango definido por la desviación estándar.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
